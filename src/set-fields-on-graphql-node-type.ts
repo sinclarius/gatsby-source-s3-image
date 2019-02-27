ea0ded9331afbc09bf1610835fe6e49e69e03ca4
Jesse Stuart <hi@jessestuart.com>
2019-02-27 14:11:34 -0500

    [cleanup]

--- src/set-fields-on-graphql-node-type.ts
+++ src/set-fields-on-graphql-node-type.ts
@@ -13,7 +13,6 @@ const {
 } = require('gatsby/graphql')
 
 export const resolveExifData = (image: S3ImageAssetNode): ExifDataType => {
-  console.log('resolve exif data', { image })
   const file = fs.readFileSync(image.absolutePath)
   const tags = exif.create(file).parse().tags
   const timestamp = tags.DateTimeOriginal * 1000
@@ -36,149 +35,36 @@ export const resolveExifData = (image: S3ImageAssetNode): ExifDataType => {
 export interface ExtendNodeTypeOptions {
   type: {
     name: string
-    nodes: any
   }
 }
 
-export default ({
-  type,
-  // pathPrefix,
-  // getNodeAndSavePathDependency,
-  // reporter,
-  // name,
-  // cache,
-}) => {
+export default ({ type }: ExtendNodeTypeOptions) => {
   if (type.name !== 'S3ImageAsset') {
     return {}
   }
 
-  const ExifData = {
-    resolve: image => {
-      // tslint:disable
-      console.log({ image })
-      // console.log('resolve exif data')
-      // const file = getNodeAndSavePathDependency(image.parent, context.path)
-      // const args = { ...fieldArgs }
-
-      return {
+  return Promise.resolve({
+    ETag: { type: GraphQLString },
+    EXIF: {
+      resolve: (image: S3ImageAssetNode) => ({
         ...type,
         ...resolveExifData(image),
-      }
-    },
-    type: new GraphQLObjectType({
-      fields: {
-        DateCreatedISO: { type: GraphQLString },
-        DateTimeOriginal: { type: GraphQLInt },
-        ExposureTime: { type: GraphQLFloat },
-        FNumber: { type: GraphQLFloat },
-        FocalLength: { type: GraphQLFloat },
-        ISO: { type: GraphQLInt },
-        LensModel: { type: GraphQLString },
-        Model: { type: GraphQLString },
-        ShutterSpeedValue: { type: GraphQLFloat },
-      },
-      name: 'EXIF',
-    }),
-  }
-
-  return Promise.resolve({
-    EXIF: {
+      }),
       type: new GraphQLObjectType({
         fields: {
-          ETag: { type: GraphQLString },
-          EXIF: ExifData,
-          Key: { type: GraphQLString },
+          DateCreatedISO: { type: GraphQLString },
+          DateTimeOriginal: { type: GraphQLInt },
+          ExposureTime: { type: GraphQLFloat },
+          FNumber: { type: GraphQLFloat },
+          FocalLength: { type: GraphQLFloat },
+          ISO: { type: GraphQLInt },
+          LensModel: { type: GraphQLString },
+          Model: { type: GraphQLString },
+          ShutterSpeedValue: { type: GraphQLFloat },
         },
-        name: 'S3ImageAssetExifData',
+        name: 'ExifData',
       }),
     },
+    Key: { type: GraphQLString },
   })
-  // return Promise.resolve({
-  //   type: new GraphQLObjectType({
-  //     name: 'ExifData',
-  //     fields: {
-  //       ETag: { type: GraphQLString },
-  //       EXIF: ExifData,
-  //       Key: { type: GraphQLString },
-  //     },
-  //     // resolve(image: S3ImageAssetNode) {
-  //     //   return Promise.resolve({
-  //     //     ...type,
-  //     //     ...resolveExifData(image),
-  //     //   })
-  //     // },
-  //   }),
 }
-
-// return {
-//   type: new GraphQLObjectType({
-//     name: 'ExifData',
-//     // tslint:disable-next-line
-//     fields: {
-//       ETag: { type: GraphQLString },
-//       EXIF: {
-//         DateCreatedISO: { type: GraphQLString },
-//         DateTimeOriginal: { type: GraphQLInt },
-//         ExposureTime: { type: GraphQLFloat },
-//         FNumber: { type: GraphQLFloat },
-//         FocalLength: { type: GraphQLFloat },
-//         ISO: { type: GraphQLInt },
-//         LensModel: { type: GraphQLString },
-//         Model: { type: GraphQLString },
-//         ShutterSpeedValue: { type: GraphQLFloat },
-//       },
-//       Key: { type: GraphQLString },
-//     },
-//     // resolve: image => {
-//     //   console.log('resolve', { image })
-//     //   // const file = getNodeAndSavePathDependency(image.parent, context.path)
-//     //   // const args = { ...fieldArgs, pathPrefix }
-//     //   return Promise.resolve({
-//     //     ...type,
-//     //     ...resolveExifData(image),
-//     //   })
-//     //   // return {
-//     //   //   ...type,
-//     //   //   ...resolveExifData(image),
-//     //   // }
-//     // },
-//   }),
-// }
-
-// export const setFieldsOnGraphQLNodeType = ({ type }): any => {
-//   if (type.name !== 'S3ImageAsset') {
-//     return {}
-//   }
-
-//   console.log('extend node type', stringify(type))
-//   return {
-//     type: {
-//       ETag: { type: GraphQLString },
-//       EXIF: {
-//         resolve: image => {
-//           console.log('resolve', { image })
-//           return {
-//             ...type,
-//             ...resolveExifData(image),
-//           }
-//         },
-//         type: new GraphQLObjectType({
-//           ExifData: {
-//             DateCreatedISO: { type: GraphQLString },
-//             DateTimeOriginal: { type: GraphQLInt },
-//             ExposureTime: { type: GraphQLFloat },
-//             FNumber: { type: GraphQLFloat },
-//             FocalLength: { type: GraphQLFloat },
-//             ISO: { type: GraphQLInt },
-//             LensModel: { type: GraphQLString },
-//             Model: { type: GraphQLString },
-//             ShutterSpeedValue: { type: GraphQLFloat },
-//           },
-//         }),
-//         // resolve(image: S3ImageAssetNode) {
-//       },
-//       Key: { type: GraphQLString },
-//     },
-//     name: 'ExifData',
-//   }
-// }
